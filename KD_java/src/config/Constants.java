1ec4af1bdab74962a6d72d37bed09b499211fda4
Tamás Rácz <tamas.racz@capgemini.com>
2019-05-08 11:06:32 +0200

    created constants

---
+++ KD_java/src/config/Constants.java
@@ -0,0 +1,21 @@
+package config;
+
+import java.io.FileInputStream;
+
+import utility.XLUtils;
+
+public class Constants 
+{
+	public static final String URL = "http://store.demoqa.com";
+	public static final String testDataPath = "C:\\Users\\taracz\\KD_java\\KD_java\\bin\\dataEngine\\DataEngine.xlsx";
+	public static final String testDataFile = "DataEngine.xlsx";
+	
+	public static final int Col_TestCaseID = 0;
+	public static final int Col_TestScenarioID = 1;
+	public static final int Col_ActionKeyword = 3;
+	
+	public static final String Sheet_TestSteps = "Test Steps";
+	
+	public static final String UserName = "testuser_3";
+	public static final String Password = "Test@123";
+}
