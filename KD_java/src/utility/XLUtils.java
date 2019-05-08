a839b4fea2ae129ea9828389ee82cee975ab4af1
Tamás Rácz <tamas.racz@capgemini.com>
2019-05-08 15:50:29 +0200

    XLUtils logging until getRowCount

--- KD_java/src/utility/XLUtils.java
+++ KD_java/src/utility/XLUtils.java
@@ -3,43 +3,66 @@ package utility;
 import java.io.FileInputStream;
 
 import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFRow;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 import config.Constants;
+import execEngine.driverScript;
 
 public class XLUtils 
 {
 	private static XSSFSheet xlWS;
 	private static XSSFWorkbook xlWB;
 	private static XSSFCell xlCell;
+	private static XSSFRow xlRow;
 	
 	public static void setExcelFile(String xlPath) throws Exception
 	{
+		try
+		{
 		FileInputStream xlFile = new FileInputStream(xlPath);
-		xlWB = new XSSFWorkbook(xlFile);		
+		xlWB = new XSSFWorkbook(xlFile);
+		}
+		catch (Exception e)
+		{
+			Log.error("Class: XLUtils | Method: setExcelFile | Exception description: " + e.getMessage());
+			driverScript.bResult = false;
+		}
 	}
 	
 	public static String getCellData(int RowNum, int ColNum, String SheetName) throws Exception
-	{
-		xlWS = xlWB.getSheet(SheetName);
+	{		
 		try
 		{
+			xlWS = xlWB.getSheet(SheetName);
 			xlCell = xlWS.getRow(RowNum).getCell(ColNum);
 			String CellData = xlCell.getStringCellValue();
 			return CellData;
 		}
 		catch (Exception e)
 		{
+			Log.error("Class: XLUtils | Method: getCellData | Exception description: " + e.getMessage());
+			driverScript.bResult = false;
 			return "";
 		}
 	}
 	
 	public static int getRowCount(String SheetName)
 	{
+		int iNumber = 0;
+		try
+		{
 		xlWS = xlWB.getSheet(SheetName);
-		int number = xlWS.getLastRowNum() + 1;
-		return number;
+		iNumber = xlWS.getLastRowNum() + 1;		
+		}
+		catch (Exception e)
+		{
+			Log.error("Class: XLUtils | Method: getRowCount | Exception description: " + e.getMessage());
+			driverScript.bResult = false;
+		}
+		
+		return iNumber;
 	}
 	
 	public static int getRowContains(String sTestCaseName, int colNum, String SheetName) throws Exception
